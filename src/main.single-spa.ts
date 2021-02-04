6034581a125a6c7f67d01658a5e90f22f0ec3f1e
Anton <mun4kin@gmail.com>
2021-02-04 13:38:19 +0300

    fix

--- src/main.single-spa.ts
+++ src/main.single-spa.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { getSingleSpaExtraProviders, singleSpaAngular } from 'single-spa-angular';
 
-const tegName = 'cv';
+const tegName = 'mainContent';
 environment.production && enableProdMode();
 
 
